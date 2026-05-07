ee9f535552a28ef3f44956d75fe074257c5dad07
Leo <leonardquispe92@gmail.com>
2026-05-07 10:06:26 -0500

    Base de datos Correcion

--- app_logistica/admin.py
+++ app_logistica/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CargoColaboradores,EstadoColaboradores,Colaboradores,TipoEstadoItems,TipoItems,TipoMoneda,TiposInsumo,TiposMovimiento
+
+
+admin.site.register(CargoColaboradores)
+admin.site.register(EstadoColaboradores)
+admin.site.register(Colaboradores)
+admin.site.register(TipoEstadoItems)
+admin.site.register(TipoItems)
+admin.site.register(TipoMoneda)
+admin.site.register(TiposInsumo)
+admin.site.register(TiposMovimiento)
