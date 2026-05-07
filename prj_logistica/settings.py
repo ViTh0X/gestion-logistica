86a5e37ef96a09802153c8334ae93af9e661cdc8
Leo <leonardquispe92@gmail.com>
2026-05-07 09:29:32 -0500

    Base de datos Correcion

--- prj_logistica/settings.py
+++ prj_logistica/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'prj_logistica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.models.backends.sqlite3',        
+        'ENGINE': 'django.db.backends.sqlite3',        
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
